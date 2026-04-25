795a66a167714b904c7d1a2a040f8c2448b04c97
326 <3263114159@qq.com>
2026-04-25 22:29:15 +0800

    写死教育背景，加速显示

---
+++ frontend/site/src/data/static-educations.js
@@ -0,0 +1,18 @@
+export const staticEducations = [
+  {
+    school: "\u6b66\u6c49\u7406\u5de5\u5927\u5b66",
+    degree: "\u7855\u58eb",
+    major: "\u8f6f\u4ef6\u5de5\u7a0b",
+    startDate: "2023-09-01",
+    endDate: "2026-06-30",
+    gpa: "4.54/5.0"
+  },
+  {
+    school: "\u6b66\u6c49\u7406\u5de5\u5927\u5b66",
+    degree: "\u672c\u79d1",
+    major: "\u4eba\u5de5\u667a\u80fd",
+    startDate: "2019-09-01",
+    endDate: "2023-06-30",
+    gpa: "GPA: 3.67/5.0"
+  }
+];
